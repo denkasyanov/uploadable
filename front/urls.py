e715871eb4cd57dd180fa611af36808f5026b100
Den Kasyanov <den@den.ai>
2018-10-05 02:41:31 +0300

    Set up urls

---
+++ front/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('add-picture/', views.AddPictureView.as_view(), name='add'),
+    path('/', views.AllPicturesView.as_view(), name='list')
+]
