8c8a7c9770a6579482d0ff50c54793138403ab54
Den Kasyanov <den@den.ai>
2018-10-05 02:38:54 +0300

    Add Picture model

--- front/models.py
+++ front/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Picture(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='pictures')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
