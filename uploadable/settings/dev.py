ced94af632935cb2703c1cbdd7e73830980c17b0
Den Kasyanov <den@den.ai>
2018-10-05 02:06:49 +0300

    Add project boilerplate

---
+++ uploadable/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+DEBUG = True
+
+SECRET_KEY = 'Very secret key'
---
+++ uploadable/settings/prod.py
@@ -0,0 +1,5 @@
+from .base import *
+
+DEBUG = False
+
+SECRET_KEY = 'Set me up'
