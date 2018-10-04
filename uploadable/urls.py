6fde4b85427b63bb250e7a797e1761d5116680f7
Den Kasyanov <den@den.ai>
2018-10-05 02:53:54 +0300

    Fix including front urls

--- uploadable/urls.py
+++ uploadable/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('front.urls'), namespace='pictures')
+    path('', include(('front.urls', 'front'), namespace='pictures'))
 ]
